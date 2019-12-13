92348e9abaa7987b124cfb92ef7a7b43f7d1984b
kuzenkovevgen <kem92@mail.ru>
2019-12-13 22:09:39 +0300

    0.1
    add level 2

--- script.js
+++ script.js
@@ -1,174 +1,3 @@
-/*function chk() {
-    var a1 = document.getElementById('a1').value * 1;
-    var a2 = document.getElementById('a2').value * 1;
-    var a3 = document.getElementById('a3').value * 1;
-    var a4 = document.getElementById('a4').value * 1;
-    var a5 = document.getElementById('a5').value * 1;
-    var a6 = document.getElementById('a6').value * 1;
-    var a7 = document.getElementById('a7').value * 1;
-    var a8 = document.getElementById('a8').value * 1;
-    var a9 = document.getElementById('a9').value * 1;
-
-    var b1 = document.getElementById('b1').value * 1;
-    var b2 = document.getElementById('b2').value * 1;
-    var b3 = document.getElementById('b3').value * 1;
-    var b4 = document.getElementById('b4').value * 1;
-    var b5 = document.getElementById('b5').value * 1;
-    var b6 = document.getElementById('b6').value * 1;
-    var b7 = document.getElementById('b7').value * 1;
-    var b8 = document.getElementById('b8').value * 1;
-    var b9 = document.getElementById('b9').value * 1;
-
-    var c1 = document.getElementById('c1').value * 1;
-    var c2 = document.getElementById('c2').value * 1;
-    var c3 = document.getElementById('c3').value * 1;
-    var c4 = document.getElementById('c4').value * 1;
-    var c5 = document.getElementById('c5').value * 1;
-    var c6 = document.getElementById('c6').value * 1;
-    var c7 = document.getElementById('c7').value * 1;
-    var c8 = document.getElementById('c8').value * 1;
-    var c9 = document.getElementById('c9').value * 1;
-
-    var d1 = document.getElementById('d1').value * 1;
-    var d2 = document.getElementById('d2').value * 1;
-    var d3 = document.getElementById('d3').value * 1;
-    var d4 = document.getElementById('d4').value * 1;
-    var d5 = document.getElementById('d5').value * 1;
-    var d6 = document.getElementById('d6').value * 1;
-    var d7 = document.getElementById('d7').value * 1;
-    var d8 = document.getElementById('d8').value * 1;
-    var d9 = document.getElementById('d9').value * 1;
-
-    var e1 = document.getElementById('e1').value * 1;
-    var e2 = document.getElementById('e2').value * 1;
-    var e3 = document.getElementById('e3').value * 1;
-    var e4 = document.getElementById('e4').value * 1;
-    var e5 = document.getElementById('e5').value * 1;
-    var e6 = document.getElementById('e6').value * 1;
-    var e7 = document.getElementById('e7').value * 1;
-    var e8 = document.getElementById('e8').value * 1;
-    var e9 = document.getElementById('e9').value * 1;
-
-    var f1 = document.getElementById('f1').value * 1;
-    var f2 = document.getElementById('f2').value * 1;
-    var f3 = document.getElementById('f3').value * 1;
-    var f4 = document.getElementById('f4').value * 1;
-    var f5 = document.getElementById('f5').value * 1;
-    var f6 = document.getElementById('f6').value * 1;
-    var f7 = document.getElementById('f7').value * 1;
-    var f8 = document.getElementById('f8').value * 1;
-    var f9 = document.getElementById('f9').value * 1;
-
-    var g1 = document.getElementById('g1').value * 1;
-    var g2 = document.getElementById('g2').value * 1;
-    var g3 = document.getElementById('g3').value * 1;
-    var g4 = document.getElementById('g4').value * 1;
-    var g5 = document.getElementById('g5').value * 1;
-    var g6 = document.getElementById('g6').value * 1;
-    var g7 = document.getElementById('g7').value * 1;
-    var g8 = document.getElementById('g8').value * 1;
-    var g9 = document.getElementById('g9').value * 1;
-
-    var h1 = document.getElementById('h1').value * 1;
-    var h2 = document.getElementById('h2').value * 1;
-    var h3 = document.getElementById('h3').value * 1;
-    var h4 = document.getElementById('h4').value * 1;
-    var h5 = document.getElementById('h5').value * 1;
-    var h6 = document.getElementById('h6').value * 1;
-    var h7 = document.getElementById('h7').value * 1;
-    var h8 = document.getElementById('h8').value * 1;
-    var h9 = document.getElementById('h9').value * 1;
-
-    var j1 = document.getElementById('j1').value * 1;
-    var j2 = document.getElementById('j2').value * 1;
-    var j3 = document.getElementById('j3').value * 1;
-    var j4 = document.getElementById('j4').value * 1;
-    var j5 = document.getElementById('j5').value * 1;
-    var j6 = document.getElementById('j6').value * 1;
-    var j7 = document.getElementById('j7').value * 1;
-    var j8 = document.getElementById('j8').value * 1;
-    var j9 = document.getElementById('j9').value * 1;
-
-    var sumA = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9;
-    var sumB = b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8 + b9;
-    var sumC = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9;
-    var sumD = d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9;
-    var sumE = e1 + e2 + e3 + e4 + e5 + e6 + e7 + e8 + e9;
-    var sumF = f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8 + f9;
-    var sumG = g1 + g2 + g3 + g4 + g5 + g6 + g7 + g8 + g9;
-    var sumH = h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
-    var sumJ = j1 + j2 + j3 + j4 + j5 + j6 + j7 + j8 + j9;
-
-    var sum1 = a1 + b1 + c1 + d1 + e1 + f1 + g1 + h1 + j1;
-    var sum2 = a2 + b2 + c2 + d2 + e2 + f2 + g2 + h2 + j2;
-    var sum3 = a3 + b3 + c3 + d3 + e3 + f3 + g3 + h3 + j3;
-    var sum4 = a4 + b4 + c4 + d4 + e4 + f4 + g4 + h4 + j4;
-    var sum5 = a5 + b5 + c5 + d5 + e5 + f5 + g5 + h5 + j5;
-    var sum6 = a6 + b6 + c6 + d6 + e6 + f6 + g6 + h6 + j6;
-    var sum7 = a7 + b7 + c7 + d7 + e7 + f7 + g7 + h7 + j7;
-    var sum8 = a8 + b8 + c8 + d8 + e8 + f8 + g8 + h8 + j8;
-    var sum9 = a9 + b9 + c9 + d9 + e9 + f9 + g9 + h9 + j9;
-
-    var diag1 = a1 + b2 + c3 + d4 + e5 + f6 + g7 + h8 + j9;
-    var diag2 = a9 + b8 + c7 + d6 + e5 + f4 + g3 + h2 + j1;
-
-    var ac13 = a1 + a2 + a3 + b1 + b2 + b3 + c1 + c2 + c3;
-    var ac46 = a4 + a5 + a6 + b4 + b5 + b6 + c4 + c5 + c6;
-    var ac79 = a7 + a8 + a9 + b7 + b8 + b9 + c7 + c8 + c9;
-
-    var df13 = d1 + d2 + d3 + e1 + e2 + e3 + f1 + f2 + f3;
-    var df46 = d4 + d5 + d6 + e4 + e5 + e6 + f4 + f5 + f6;
-    var df79 = d7 + d8 + d9 + e7 + e8 + e9 + f7 + f8 + f9;
-
-    var gj13 = g1 + g2 + g3 + h1 + h2 + h3 + j1 + j2 + j3;
-    var gj46 = g4 + g5 + g6 + h4 + h5 + h6 + j4 + j5 + j6;
-    var gj79 = g7 + g8 + g9 + h7 + h8 + h9 + j7 + j8 + j9;
-
-    if (sumA == 45 &&
-        sumB == 45 &&
-        sumC == 45 &&
-        sumD == 45 &&
-        sumE == 45 &&
-        sumF == 45 &&
-        sumG == 45 &&
-        sumH == 45 &&
-        sumJ == 45 &&
-        sum1 == 45 &&
-        sum2 == 45 &&
-        sum3 == 45 &&
-        sum4 == 45 &&
-        sum5 == 45 &&
-        sum6 == 45 &&
-        sum7 == 45 &&
-        sum8 == 45 &&
-        sum9 == 45 &&
-        diag1 == 45 &&
-        diag2 == 45 &&
-        ac13 == 45 &&
-        ac46 == 45 &&
-        ac79 == 45 &&
-        df13 == 45 &&
-        df46 == 45 &&
-        df79 == 45 &&
-        gj13 == 45 &&
-        gj46 == 45 &&
-        gj79 == 45
-    ) {
-        chkstr.innerHTML = "OK";
-    }
-    else {
-        chkstr.innerHTML = "BAD";
-    }
-
-    console.log(sumA, sumB, sumC, sumD, sumE, sumF, sumG, sumH, sumJ,
-        sum1, sum2, sum3, sum4, sum5, sum6, sum7, sum8, sum9,
-        diag1, diag2,
-        ac13, ac46, ac79, df13, df46, df79, gj13, gj46, gj79);
-}
-*/
-
-
-
 function chk() {                            //запуск скрипта
     var table = new Array(n);               //создаем массив
     var n = 9;                              //задаем количество строк и столбцов поля
@@ -410,7 +239,6 @@ function clr() {
     var inputs = document.getElementsByTagName('input');
     for (let i = 0; i < inputs.length; i++) {
         let inputsI = document.getElementsByTagName('input')[i];
-        // console.log(inputsI);
         inputsI.value = '';
     }
 }
@@ -428,12 +256,9 @@ var arrLevel1 =
 
 
 function lvl1() {
+    clr();
     res.innerHTML = "";
     var inputs = document.getElementsByTagName('input');
-    for (let i = 0; i < inputs.length; i++) {
-        let inputsI = document.getElementsByTagName('input')[i];
-        inputsI.value = '';
-    }
     for (let i = 0; i < inputs.length; i = i + 2) {
         let inputsI = document.getElementsByTagName('input')[i];
         inputsI.value = arrLevel1[i];
@@ -446,5 +271,36 @@ function lvl1right() {
     for (let i = 0; i < inputs.length; i++) {
         let inputsI = document.getElementsByTagName('input')[i];
         inputsI.value = arrLevel1[i];
+    }
+}
+
+var arrLevel2 =
+    [4, 7, 8, 1, 9, 3, 5, 6, 2,
+        2, 9, 1, 8, 6, 5, 4, 7, 3,
+        6, 3, 5, 4, 7, 2, 8, 9, 1,
+        8, 1, 7, 6, 2, 4, 9, 3, 5,
+        9, 2, 4, 5, 3, 7, 1, 8, 6,
+        5, 6, 3, 9, 1, 8, 7, 2, 4,
+        3, 4, 6, 7, 8, 1, 2, 5, 9,
+        7, 5, 9, 2, 4, 6, 3, 1, 8,
+        1, 8, 2, 3, 5, 9, 6, 4, 7];
+
+
+function lvl2() {
+    clr();
+    res.innerHTML = "";
+    var inputs = document.getElementsByTagName('input');
+    for (let i = 0; i < inputs.length; i = i + 2) {
+        let inputsI = document.getElementsByTagName('input')[i];
+        inputsI.value = arrLevel2[i];
+    }
+}
+
+function lvl2right() {
+    res.innerHTML = "";
+    var inputs = document.getElementsByTagName('input');
+    for (let i = 0; i < inputs.length; i++) {
+        let inputsI = document.getElementsByTagName('input')[i];
+        inputsI.value = arrLevel2[i];
     }
 }
